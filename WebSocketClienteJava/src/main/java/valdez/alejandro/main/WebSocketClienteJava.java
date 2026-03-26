4bdd51b630c85d0d00379bb96fd775d92165a772
janotheboss75 <82925930+janotheboss75@users.noreply.github.com>
2026-03-26 00:20:05 -0700

    PROYECTO CLIENT JAVA

---
+++ WebSocketClienteJava/src/main/java/valdez/alejandro/main/WebSocketClienteJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package valdez.alejandro.main;
+
+/**
+ *
+ * @author janot
+ */
+public class WebSocketClienteJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
